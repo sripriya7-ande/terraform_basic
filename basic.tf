18d187a5835d6eaebb77d08cd15112bd9e76590c
sripriya7-ande <sripriyaande@outlook.com>
2025-06-10 21:47:12 +0530

    Update basic.tf

--- basic.tf
+++ basic.tf
@@ -1,9 +1,4 @@
-resource "local_file" "pet" {
-filename = "pets.txt"
-content = "we love pets!"
-}
-resource "random_pet" "mypet" {
-prefix = "MR"
-separator = "."
-length = "1"
+resource "local_file" "my_text_file" {
+  filename = "pets.txt"
+  content = "I love my dog"
 }
